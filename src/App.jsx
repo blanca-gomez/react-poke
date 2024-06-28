6fdef5f663d5bd97d2f73e909077fbcd2f761671
Blanca Gómez <blanca_gp_96@hotmail.com>
2024-06-28 13:17:51 +0200

    pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import './index.css';
+import pokemonLogo from '../public/images/pokemon.png'
 
 
 function App () {
   const [nombre, setNombre] = useState('')
   const [pokemon, setPokemon] = useState(null)
+  const [error,setError]= useState('')
+
+  useEffect(() => {
+    if(nombre){
+      getPokemon(nombre)
+    }
+  }, [nombre])
 
   const getPokemon = async (name) => {
     try{
+      setError('');
       const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${name}`);
       if(!res.ok){
         throw new Error('Pokemon no encontrado')
       }
       const data = await res.json()
       setPokemon(data)
-    }catch(error){
-      console.log(error)
+    }catch (error) {
+      setPokemon(null)
+      setError(error.message)
     }  
   }
 
@@ -27,6 +37,9 @@ function App () {
 
   return(
     <>
+      <div className='pokemon-name'>
+        <img src='../public/images/pokemon2.png'></img>
+      </div>
       <form onSubmit={handleSubmit}>
         <label htmlFor='nombre'>Nombre del pokemon</label>
         <input
@@ -38,29 +51,28 @@ function App () {
         />
         <button type='submit'>Buscar</button>
       </form>
+      {error && <h2 className='error'>Pokemon no encontrado</h2>}
 
       {pokemon && (
-      <div className='card'>
-        <h1>{pokemon.name}</h1>
-        <img src={pokemon.sprites.front_default} alt='pokemon name' className='logo'/>
-        <h2>Altura: {pokemon.height} dm</h2>
-        <h2>Peso: {pokemon.weight} hg</h2>
-        <h2>Habilidades: </h2>
-        <ul>
-          {pokemon.abilities.map((ability, index) => (
-            <li key={index}>{ability.ability.name}</li>
-          ))}
-        </ul>
-      
-      
-      </div>
-    )}
+        <div className='card'>
+          <h1>{pokemon.name}</h1>
+          <img src={pokemon.sprites.front_default} alt='pokemon name' className='logo'/>
+          <div className='card-characteristics'>
+            <h2>Altura: {pokemon.height} dm</h2>
+            <h2>Peso: {pokemon.weight} hg</h2>
+            <div class= 'card-abilities'>
+              <h2>Habilidades: </h2>
+              <ul>
+                {pokemon.abilities.map((ability, index) => (
+                  <li key={index}>{ability.ability.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+       )}
     </>
-    
-
-
   )
-  
 };
 
 export default App;
